378ac6ddbe265c1ef0b2830a2dd06c08f41c5f32
Ian Andrewson <ianandrewson@gmail.com>
2019-10-07 13:59:15 -0700

    added products page and skeleton product

---
+++ app.js
@@ -0,0 +1,16 @@
+// Property 	Description
+// id 	a unique string that identifies this product
+// name 	user friendly name of the product
+// image 	image file name for this image (should live in assets)
+// description 	a longer description of the product
+// category 	the category this product belongs to, limit to one
+// price 	the price the user will pay
+
+// let product = {
+//     id: "blah",
+//     name: 'blah',
+//     image: 'blah',
+//     description: 'blah',
+//     category: 'blah',
+//     price: 'blah'
+// };
