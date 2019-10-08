fb948069afdc7f29cb0324b7a39f0e9bcc74b1ae
Ian Andrewson <ianandrewson@gmail.com>
2019-10-08 13:27:53 -0700

    updated file hierarchy; added shopping cart folder

--- Products/instruments.js
+++ Products/instruments.js
@@ -1,7 +1,7 @@
 const ms20 = {
     id: 'ms20',
     name: 'Korg MS-20',
-    image: './assests/ms20.png',
+    image: '../assests/ms20.png',
     description: 'Analog Subtractive Synthesizer',
     category: 'Synth',
     price: '$400.00'
@@ -10,7 +10,7 @@ const ms20 = {
 const arp2600 = {
     id: 'arp2600',
     name: 'Arp 2600',
-    image: './assests/arp2600.png',
+    image: '../assests/arp2600.png',
     description: 'Rare Vintage Analog Synthesizer',
     category: 'Synth',
     price: '$10000.00'
@@ -19,7 +19,7 @@ const arp2600 = {
 const deckards = {
     id: 'deckards',
     name: "Black Corporation Deckard's Dream",
-    image: './assests/deckards.png',
+    image: '../assests/deckards.png',
     description: 'Analog Phase Distortion Synthesizer',
     category: 'Synth',
     price: '$3800.00'
@@ -28,7 +28,7 @@ const deckards = {
 const digitoneKeys = {
     id: 'digitoneKeys',
     name: 'Elektron Digitone Keys',
-    image: './assests/digitonekeys.png',
+    image: '../assests/digitonekeys.png',
     description: 'Digital Frequency Modulation Synthesizer',
     category: 'Synth',
     price: '$1300.00'
@@ -37,7 +37,7 @@ const digitoneKeys = {
 const hydra = {
     id: 'hydra',
     name: 'ASM Hydrasynth',
-    image: './assests/hydra.png',
+    image: '../assests/hydra.png',
     description: 'Morphing Wavetable Synthesizer',
     category: 'Synth',
     price: '$1300.00'
@@ -46,7 +46,7 @@ const hydra = {
 const juno106 = {
     id: 'juno',
     name: 'Roland Juno-106',
-    image: './assests/juno106.png',
+    image: '../assests/juno106.png',
     description: 'Analog Subtractive Synthesizer',
     category: 'Synth',
     price: '$1800.00'
@@ -55,7 +55,7 @@ const juno106 = {
 const moogOne = {
     id: 'moogOne',
     name: 'Moog One',
-    image: './assests/moogone.png',
+    image: '../assests/moogone.png',
     description: 'Analog Synthesizer',
     category: 'Synth',
     price: '$8000.00'
@@ -64,7 +64,7 @@ const moogOne = {
 const polyEvolver = {
     id: 'polyEvolver',
     name: 'Dave Smith Instruments Poly Evolver',
-    image: './assests/polyevolver.png',
+    image: '../assests/polyevolver.png',
     description: 'Hybrid Synthesizer',
     category: 'Synth',
     price: '$2500.00'
@@ -73,7 +73,7 @@ const polyEvolver = {
 const roland808 = {
     id: 'roland808',
     name: 'Roland TR-808',
-    image: './assests/roland808.png',
+    image: '../assests/roland808.png',
     description: 'Analog Drum Machine',
     category: 'Drum Machine',
     price: '$4000.00'
@@ -82,7 +82,7 @@ const roland808 = {
 const superSix = {
     id: 'superSix',
     name: 'UDO Super 6',
-    image: './assests/supersix.png',
+    image: '../assests/supersix.png',
     description: 'Hybrid Binaural Supersaw Synthesizer',
     category: 'Synth',
     price: '$2800.00'
