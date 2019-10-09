4700672727ce4253b8a7ab06b51eb4cc52b03cfa
Ian Andrewson <ianandrewson@gmail.com>
2019-10-09 08:47:42 -0700

    shopping cart order fully renders

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,13 +1,23 @@
+import { instrumentList } from '../Products/instruments.js';
+import { renderTableRow } from './render-table-row.js';
+import * as utils from '../common/utils.js';
+
 export const cart = [{
     id: 'ms20',
-    quantity: 1
+    quantity: 2
 }, {
     id: 'roland808',
     quantity: 1
 }, {
     id: 'polyEvolver',
-    quantity: 1
+    quantity: 3
 }, {
     id: 'superSix',
     quantity: 1
 }];
+
+const shoppingCartTable = document.querySelector('tbody');
+for (let i = 0; i < cart.length; i++) {
+    let boughtProduct = utils.findById(instrumentList, cart[i].id);
+    shoppingCartTable.appendChild(renderTableRow(boughtProduct, cart[i]));
+}
